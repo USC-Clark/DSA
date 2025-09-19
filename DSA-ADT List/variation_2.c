bc28300e924db7393e97536451dcb75c8395db84
basilgo-jay <21102868@usc.edu.ph>
2025-09-20 02:31:58 +0800

    var2 finished

--- DSA-ADT List/variation_2.c
+++ DSA-ADT List/variation_2.c
@@ -1,37 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 10
 
-// void initialize(EPtr L){
+typedef struct{
+    int elem[MAX];
+    int count;
+}Etype, *EPtr;
 
-// }
+void initialize(EPtr L){
+    L->count = 0;
+}
 
-// void insertPos(EPtr L, int data, int position){
+void insertPos(EPtr L, int data, int position){
 
-// }
+    if(position>L->count){
+        printf("Position Invalid");
+    }else if(L->count == MAX){
+        printf("Array is Full!");
+    }else{
+        for(int i=L->count-1;i>=position;i--){
+            L->elem[i+1] = L->elem[i];
+        }
+        L->elem[position] = data;
+        L->count++;
+    }
+}
 
-// void deletePos(EPtr L, int position){
+void deletePos(EPtr L, int position){
 
-// }
+    if(position>=L->count){
+        printf("Invalid Position");
+    }else{
+        for(int i=position;i<L->count;i++){
+            L->elem[i] = L->elem[i+1];
+        }
+        L->count--;
+    }
+}
 
-// int locate(EPtr L, int data){
+int locate(EPtr L, int data){
 
-// }
+    for(int i=0;i<L->count;i++){
+        if(L->elem[i]==data){
+            return i;
+        }
+    }
+    return -1;
+}
 
-// int retrieve(EPtr L, int position){
+int retrieve(EPtr L, int position){
 
-// }
+    if(position<=L->count){
+        return L->elem[position];
+    }else{
+        printf("Invalid Position");
+    }
+}
 
-// void insertSorted(EPtr L, int data){
+void insertSorted(EPtr L, int data){
 
-// }
+    if(L->count!=MAX){
+        for(int i=0;i<L->count;i++){
+            if(L->elem[i]<data && L->elem[i+1]>data){
+                for(int j=L->count;j>i;j--){
+                    L->elem[j] = L->elem[j-1];
+                }
+                L->elem[i+1] = data;
+                L->count++;
+                break;
+            }
+        }
+    }
+}
 
-// void display(EPtr L){
+void display(EPtr L){
 
-// }
+    for(int i=0;i<L->count;i++){
+        printf("%d ", L->elem[i]);
+    }
+}
 
-// void makeNull(EPtr L){
+void makeNull(EPtr L){
 
-// }
+    free(L);
+}
+
 int main(){
 
+    EPtr L = malloc(sizeof(EPtr));
+
+    int hold;
+
+    initialize(L);
+    insertPos(L, 1, 0);
+    insertPos(L, 2, 1);
+    insertPos(L, 3, 2);
+    insertPos(L, 5, 3);
+    //deletePos(L, 1);
+    // hold = locate(L, 4);
+    insertSorted(L, 4);
+    // hold = retrieve(L, 2);
+    // printf("%d\n", hold);
+    display(L);
 }
